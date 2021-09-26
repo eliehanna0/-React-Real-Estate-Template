638dfb3daa11511ce35ef15a5b32f9540034f2e3
elie <eliehanna0000@gmail.com>
2021-09-26 14:53:12 -0400

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./css/App.css";
 import Header from "./components/header/header";
 import PropertySingle from "./components/properties/property.single";
 import Home from "./components/home/home";
+import NotFound from "./components/_404";
 import About from "./components/about/about";
 import Contact from "./components/contact/contact";
 import Head from "./components/common/head";
@@ -42,6 +43,10 @@ const App = () => {
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
           <Route path="/property/:id" component={PropertySingle} />
+          <Route path="/not-found">
+            <NotFound />
+          </Route>
+          <Redirect to="/not-found" />
         </Switch>
       </MuiThemeProvider>
     </React.Fragment>
---
+++ src/components/_404.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+
+const NotFound = () => {
+  return (
+    <React.Fragment >
+      <Container style={{textAlign:"center", flexGrow:1}}>
+          <h1>Page Not Found</h1>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default NotFound;
